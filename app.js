d28f79d3f1c60820082eca2fe3312a8244350de2
“peifen-wu” <wuplaystation@gmail.com>
2016-07-25 15:28:13 +0800

    add socket

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const app = require('express')();
 const http = require('http').Server(app)
-
+const io = require('socket.io')(http);
 
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+    console.log('a user connected')
 })
 
 http.listen(3000, function(){
