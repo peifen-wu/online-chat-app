6537897b22c4c726db161b5dc3d8480b32b0be57
“peifen-wu” <wuplaystation@gmail.com>
2016-07-25 16:01:19 +0800

    print message out

--- app.js
+++ app.js
@@ -1,20 +1,17 @@
-const app = require('express')();
+const app = require('express')()
 const http = require('http').Server(app)
-const io = require('socket.io')(http);
-
+const io = require('socket.io')(http)
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname + '/index.html')
-})
+  res.sendFile(__dirname + '/index.html')
+});
 
 io.on('connection', function(socket){
-    console.log('a user connected')
-    socket.on('disconnect', function(){
-        console.log('user disconnect')
-    })
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg)
+  })
 })
 
 http.listen(3000, function(){
-    console.log('listening on *:3000')
+  console.log('listening on *:3000');
 })
-
